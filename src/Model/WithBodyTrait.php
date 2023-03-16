b01c50af153bbe46da548d0c3bb8ae793477e541
tienvx <tien.xuan.vo@gmail.com>
2023-03-16 09:48:21 +0700

    Check content type not null before setting body

--- src/Model/WithBodyTrait.php
+++ src/Model/WithBodyTrait.php
@@ -8,7 +8,8 @@ trait WithBodyTrait
 {
     protected function withBody(int $interaction, int $part, ?string $contentType = null, ?string $body = null): void
     {
-        if (is_null($body)) {
+        if (is_null($body) || is_null($contentType)) {
+            // Pact Plugin require content type to be set, or it will panic.
             return;
         }
         $error = $this->ffi->pactffi_interaction_contents($interaction, $part, $contentType, $body);
