fc48766836f41e2c0482ab5d8b5dd9d3f5ae0cfd
tienvx <tien.xuan.vo@gmail.com>
2023-03-20 21:32:05 +0700

    Add message builders

---
+++ src/MessageBuilder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tienvx\PactPhpPlugin;
+
+use PhpPact\Consumer\Driver\MessageDriverInterface;
+use PhpPact\Consumer\Model\Message;
+use PhpPact\Consumer\MessageBuilder as BaseMessageBuilder;
+
+class MessageBuilder extends BaseMessageBuilder
+{
+    private Message $message;
+
+    public function __construct(MessageDriverInterface $driver)
+    {
+        $this->message = new Message();
+        $this->driver  = $driver;
+    }
+}
